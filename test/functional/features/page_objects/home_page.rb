566304fae8014c172e23663e4735c0f004073673
SorenTheBrave <hanseas11@juniata.edu>
2015-07-15 16:29:56 -0400

    getting rid of extra files

--- test/functional/features/page_objects/home_page.rb
+++
@@ -1,7 +0,0 @@
-class HomePage
-  include PageObject
-  #Add site common objects (ex. Navbars)
-
-  #Add page specific elements (ex. Buttons, textfields)
-
-end
--- test/functional/features/page_objects/login_page.rb
+++
@@ -1,7 +0,0 @@
-class LoginPage
-  include PageObject
-  #Add site common objects (ex. Navbars)
-
-  #Add page specific elements (ex. Buttons, textfields)
-
-end
