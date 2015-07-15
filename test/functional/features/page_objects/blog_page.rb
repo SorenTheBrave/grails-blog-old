fa817f048b54deefcfb8bbec7b53db9eee4d1ce1
TheCalebThornton <cthornton777@yahoo.com>
2015-07-15 16:41:23 -0400

    Search Blog Posts
    Scenario: Search for blog post
    add to blog_page
    create search_blog_steps

--- test/functional/features/page_objects/blog_page.rb
+++ test/functional/features/page_objects/blog_page.rb
@@ -3,21 +3,42 @@ require 'page-object'
 class BlogPage
   include PageObject
 
-  links(:posts, :class => 'postLink')
-
+  links(:post_links, :class => 'postLink')
+  text_field (:search_field,:class => 'search')
+  button(:search_btn, :class => 'searchButton')
+  h2(:postTitles, :class => 'postTitles')
 
   def goToBlogger (blogger)
     visit_page 'http://localhost:8080/grails-blog/' + blogger
   end
 
-  def getPosts
-    dates = []
-    links.each do |i|
-      dates[i] = getDate(links[i])
-    end
+  def searchPosts (search)
+    search_field.value = search
+    clickSearch
+  end
+  def clickSearch
+    search_btn.click()
   end
 
+  def postsOrdered
+    dates = getPostDates
+    compareDates(dates)
+  end
+  def getPostDates
+    dates = []
+    post_links.each do |i|
+      dates[i] = getDate(links[i])
+    end
+    return dates
+  end
   def getDate (link)
     date = link.html.scan("\d{2}/\d{2}/\d{4}")
+  end
+  def compareDates (dates)
+    valid = false
+    dates.each do |i=1|
+      valid = (dates[i].jd < dates[i-1])
+    end
+    return valid
   end
 end
