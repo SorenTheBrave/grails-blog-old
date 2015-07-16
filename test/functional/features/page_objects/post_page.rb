16b088af6722ee84772298c4695e769882679a53
TheCalebThornton <cthornton777@yahoo.com>
2015-07-16 12:16:53 -0400

    ATDD Reading comments
    post_page - added some methods for comment section
    read_comments_steps - wrote step for checking if comments were displayed
    on page

--- test/functional/features/page_objects/post_page.rb
+++ test/functional/features/page_objects/post_page.rb
@@ -5,8 +5,18 @@ class PostPage
 
   page_url "http://localhost:8080/grails-blog/user/post1"
 
+  link(:expand_comment_link, :id => 'Expand Comments')
+  div(:comments, :id => 'Comment Container')
+
   def get_post_subject
     @browser.h1.text
+  end
+  def comments_exist?
+    click_element(expand_comment_link)
+    comments.visible?
+  end
+  def click_element (element)
+    element.click
   end
 
 end
